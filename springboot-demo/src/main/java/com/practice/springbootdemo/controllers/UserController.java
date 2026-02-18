2d9532fca830f19c88ac37a40f3f37db0db5c2ac
Saran Sanjay <saransanjay96@gmail.com>
2026-02-18 23:28:32 +0530

    MySql Connection, User Entity, User CRUD, ResouceNotFound Exception Handling,

--- springboot-demo/src/main/java/com/practice/springbootdemo/controllers/UserController.java
+++ springboot-demo/src/main/java/com/practice/springbootdemo/controllers/UserController.java
@@ -1,16 +1,70 @@
 package com.practice.springbootdemo.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.practice.springbootdemo.entity.UserEntity;
+import com.practice.springbootdemo.exceptions.ResourceNotFoundException;
+import com.practice.springbootdemo.repository.UserRepository;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+//	@GetMapping
+//	public String getUser()
+//	{
+//		return "Welcome to Programming World";
+//	}
+	
+	
 	@GetMapping
-	public String getUser()
+	public List<UserEntity> getUsers()
 	{
-		return "Welcome to Programing World";
+		return userRepository.findAll();
 	}
+	
+	@GetMapping("/{id}")
+	public UserEntity getUserById(@PathVariable Long id) {
+		return userRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("User not founf with this id:"+id));
+	}
+	
+	@PostMapping
+	public UserEntity createUser(@RequestBody UserEntity user)
+	{
+		return userRepository.save(user);		
+	}
+	
+	@PutMapping("/{id}")
+	public UserEntity updateUser(@PathVariable Long id,@RequestBody UserEntity user)
+	{
+		UserEntity userData = userRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("User not founf with this id:"+id));
+		userData.setEmail(user.getEmail());
+		userData.setName(user.getName());
+		return userRepository.save(userData);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteUser(@PathVariable Long id)
+	{
+		UserEntity userData = userRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("User not founf with this id:"+id));
+		userRepository.delete(userData);
+		return ResponseEntity.ok().build();
+	}
+	
+	
 }
---
+++ springboot-demo/src/main/java/com/practice/springbootdemo/entity/UserEntity.java
@@ -0,0 +1,44 @@
+package com.practice.springbootdemo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class UserEntity {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public UserEntity(Long id, String name, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+	public UserEntity() {
+		super();
+	}
+}
